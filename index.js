c90a46d30fd9261ff56b3ab41049fce232ba3071
James McManamey <jfmcmanamey@gmail.com>
2018-08-12 23:12:30 -0700

    add commander example for option and command

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 #!/usr/bin/env node
 
-const args = process.argv
-let response = ''
+const program = require('commander');
 
-if (args[2]=='red'){
-    response = 'alret, alert!'
-}
-else{
-    response = 'All clear'
-}
-console.log(response)
-console.log(process.argv)
+program
+    .version('0.1.0')
+    .option('-c --config','configure trello API key')
+    .action(()=>{
+        console.log('options loaded')
+    })
+
+program
+  .command('setup [env]')
+  .description('run setup commands for all envs')
+  .option("-s, --setup_mode [mode]", "Which setup mode to use")
+  .action(function(env, options){
+    var mode = options.setup_mode || "normal";
+    env = env || 'all';
+    console.log('setup for %s env(s) with %s mode', env, mode);
+  });
+
+
+
+
+/* parse inputs and pass to commander */
+
+program.parse(process.argv);
+
+
+/* Show help text if no arguments are passed in*/
+
+if (!program.args.length) program.help();
